235fc3f312c6f63cf4bc3393a300f31936d169a2
Muhammad Khalil <pkhalisoft@gmail.com>
2023-05-16 00:22:40 +0100

    fixed a bit

--- screens/GetStartedScreen.js
+++ screens/GetStartedScreen.js
@@ -1,114 +1,135 @@
-import React, { useState } from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import axios from 'axios';
-import { encode } from 'base-64';
+import React, { useState } from "react";
+import {
+	View,
+	Text,
+	Image,
+	StyleSheet,
+	TouchableOpacity,
+	TextInput,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import { encode } from "base-64";
 
 const GetStartedScreen = () => {
-  const navigation = useNavigation();
-  const [phoneNumber, setPhoneNumber] = useState('');
+	const navigation = useNavigation();
+	const [phoneNumber, setPhoneNumber] = useState("");
 
-  const generateOTP = () => {
-    // Generate a random 4-digit OTP
-    return Math.floor(1000 + Math.random() * 9000).toString();
-  };
+	const generateOTP = () => {
+		// Generate a random 4-digit OTP
+		return Math.floor(1000 + Math.random() * 9000).toString();
+	};
 
-  const handleButtonClick = async () => {
-    if (phoneNumber === '') {
-      console.log('Please enter a phone number');
-      return;
-    }
+	const handleButtonClick = async () => {
+		if (phoneNumber === "") {
+			console.log("Please enter a phone number");
+			return;
+		}
 
-    const generatedOTP = generateOTP(); // Generate OTP
+		const generatedOTP = generateOTP(); // Generate OTP
 
-    try {
-      const accountSid = 'AC05de73e04d00099ddf28737cab1b507e';
-      const authToken = '3a76e8ae4ea5146a86a6717bf8a4d6b0';
-      const credentials = `${accountSid}:${authToken}`;
-      const encodedCredentials = encode(credentials);
+		try {
+			// const accountSid = "AC05de73e04d00099ddf28737cab1b507e";
+			// const authToken = "3a76e8ae4ea5146a86a6717bf8a4d6b0";
+			const accountSid = "ACa0b17565409d340f2cbd3906b0dfca39";
+			const authToken = "f93afb38d49f015a1eef0303b9f87753";
+			const credentials = `${accountSid}:${authToken}`;
+			const encodedCredentials = encode(credentials);
 
-      const response = await axios.post(
-        'https://api.twilio.com/2010-04-01/Accounts/AC05de73e04d00099ddf28737cab1b507e/Messages.json',
-        {
-          Body: `Your OTP is: ${generatedOTP}`,
-          From: '+12057724716',
-          To: phoneNumber,
-        },
-        {
-          headers: {
-            Authorization: `Basic ${encodedCredentials}`,
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-        }
-      );
+			// console.log({
+			// 	phone_number: phoneNo,
+			// 	role: "rider",
+			// });
+			const response = await axios.post(
+				"https://api.saferide.com.ng/authentication/signup/initialize",
+				{
+					phone_number: phoneNumber,
+					role: "rider",
+				}
+			);
+			// const response = await axios.post(
+			// 	"https://api.twilio.com/2010-04-01/Accounts/ACa0b17565409d340f2cbd3906b0dfca39/Messages.json",
+			// 	{
+			// 		Body: `Your OTP is: ${generatedOTP}`,
+			// 		From: "",
+			// 		To: phoneNumber,
+			// 	},
+			// 	{
+			// 		headers: {
+			// 			Authorization: `Basic ${encodedCredentials}`,
+			// 			"Content-Type": "application/x-www-form-urlencoded",
+			// 		},
+			// 	}
+			// );
 
-      console.log(response.data);
+			console.log(response.data);
 
-      // Navigate to the next screen
-      navigation.navigate('CodeVerification');
-    } catch (error) {
-      console.log('There is an error:', error.response.data);
-    }
-  };
+			// Navigate to the next screen
+			navigation.navigate("CodeVerification");
+		} catch (error) {
+			console.log("There is an error:", error);
+			// console.log("There is an error:", error.response.data);
+		}
+	};
 
-  return (
-    <View style={styles.container}>
-      <Image source={require('../assets/snowy.jpg')} style={styles.image} />
-      <Text style={styles.label}>Enter Phone Number:</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={setPhoneNumber}
-        value={phoneNumber}
-        keyboardType="phone-pad"
-      />
-      <TouchableOpacity style={styles.button} onPress={handleButtonClick}>
-        <Text style={styles.buttonText}>Get Started</Text>
-      </TouchableOpacity>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Image source={require("../assets/snowy.jpg")} style={styles.image} />
+			<Text style={styles.label}>Enter Phone Number:</Text>
+			<TextInput
+				style={styles.input}
+				onChangeText={setPhoneNumber}
+				value={phoneNumber}
+				keyboardType="phone-pad"
+			/>
+			<TouchableOpacity style={styles.button} onPress={handleButtonClick}>
+				<Text style={styles.buttonText}>Get Started</Text>
+			</TouchableOpacity>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#E0E0E0',
-  },
-  image: {
-    marginTop: -150,
-    width: 500,
-    height: 500,
-    borderBottomRightRadius: 300,
-    marginBottom: 20,
-  },
-  label: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  input: {
-    width: '70%',
-    height: 40,
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 8,
-    paddingHorizontal: 10,
-    marginBottom: 20,
-  },
-  button: {
-    backgroundColor: '#007AFF',
-    borderRadius: 8,
-    paddingVertical: 20,
-    paddingHorizontal: 20,
-    width: '70%',
-  },
-  buttonText: {
-    color: '#FFFFFF',
-    fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "#E0E0E0",
+	},
+	image: {
+		marginTop: -150,
+		width: 500,
+		height: 500,
+		borderBottomRightRadius: 300,
+		marginBottom: 20,
+	},
+	label: {
+		fontSize: 16,
+		fontWeight: "bold",
+		marginBottom: 10,
+	},
+	input: {
+		width: "70%",
+		height: 40,
+		borderWidth: 1,
+		borderColor: "gray",
+		borderRadius: 8,
+		paddingHorizontal: 10,
+		marginBottom: 20,
+	},
+	button: {
+		backgroundColor: "#007AFF",
+		borderRadius: 8,
+		paddingVertical: 20,
+		paddingHorizontal: 20,
+		width: "70%",
+	},
+	buttonText: {
+		color: "#FFFFFF",
+		fontSize: 16,
+		fontWeight: "bold",
+		textAlign: "center",
+	},
 });
 
 export default GetStartedScreen;
--- screens/OTPScreen.js
+++ screens/OTPScreen.js
@@ -1,134 +1,145 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { View, Text, TextInput, Button, StyleSheet } from "react-native";
 import { PermissionsAndroid } from "react-native";
 import {
-  startReadSMS,
-  stopReadSMS,
+	startReadSMS,
+	stopReadSMS,
 } from "@maniac-tech/react-native-expo-read-sms";
 
 const OTPScreen = () => {
-  const [verificationCode, setVerificationCode] = useState(['', '', '', '']);
-  const [hasReceiveSMSPermission, setHasReceiveSMSPermission] = useState(false);
-  const [hasReadSMSPermission, setHasReadSMSPermission] = useState(false);
+	const [verificationCode, setVerificationCode] = useState(["", "", "", ""]);
+	const [hasReceiveSMSPermission, setHasReceiveSMSPermission] = useState(false);
+	const [hasReadSMSPermission, setHasReadSMSPermission] = useState(false);
 
-  const handleVerificationCodeChange = (index, value) => {
-    const updatedVerificationCode = [...verificationCode];
-    updatedVerificationCode[index] = value;
-    setVerificationCode(updatedVerificationCode);
-  };
+	const handleVerificationCodeChange = (index, value) => {
+		const updatedVerificationCode = [...verificationCode];
+		updatedVerificationCode[index] = value;
+		setVerificationCode(updatedVerificationCode);
+	};
 
-  const handleVerifyButton = () => {
-    const code = verificationCode.join('');
-    // Verify the entered code
-    // You can implement your code verification logic here
-  };
+	const handleVerifyButton = () => {
+		const code = verificationCode.join("");
+		// Verify the entered code
+		// You can implement your code verification logic here
+	};
 
-  useEffect(() => {
-    const checkPermissions = async () => {
-      const customHasReceiveSMSPermission = await PermissionsAndroid.check(
-        PermissionsAndroid.PERMISSIONS.RECEIVE_SMS
-      );
-      const customHasReadSMSPermission = await PermissionsAndroid.check(
-        PermissionsAndroid.PERMISSIONS.READ_SMS
-      );
+	useEffect(() => {
+		const checkPermissions = async () => {
+			const customHasReceiveSMSPermission = await PermissionsAndroid.check(
+				PermissionsAndroid.PERMISSIONS.RECEIVE_SMS
+			);
+			const customHasReadSMSPermission = await PermissionsAndroid.check(
+				PermissionsAndroid.PERMISSIONS.READ_SMS
+			);
 
-      setHasReceiveSMSPermission(customHasReceiveSMSPermission);
-      setHasReadSMSPermission(customHasReadSMSPermission);
-    };
+			setHasReceiveSMSPermission(customHasReceiveSMSPermission);
+			setHasReadSMSPermission(customHasReadSMSPermission);
+		};
 
-    const requestSMSPermission = async () => {
-      try {
-        const granted = await PermissionsAndroid.requestMultiple([
-          PermissionsAndroid.PERMISSIONS.READ_SMS,
-          PermissionsAndroid.PERMISSIONS.RECEIVE_SMS,
-        ]);
+		const requestSMSPermission = async () => {
+			await PermissionsAndroid.requestMultiple([
+				PermissionsAndroid.PERMISSIONS.READ_SMS,
+				PermissionsAndroid.PERMISSIONS.RECEIVE_SMS,
+			]);
 
-        if (
-          granted['android.permission.READ_SMS'] === PermissionsAndroid.RESULTS.GRANTED &&
-          granted['android.permission.RECEIVE_SMS'] === PermissionsAndroid.RESULTS.GRANTED
-        ) {
-          console.log('SMS permission granted');
-          checkPermissions();
-        } else {
-          console.log('SMS permission denied');
-        }
-      } catch (error) {
-        console.log('Error requesting SMS permission:', error);
-      }
-    };
+			try {
+				const granted = await PermissionsAndroid.requestMultiple([
+					PermissionsAndroid.PERMISSIONS.READ_SMS,
+					PermissionsAndroid.PERMISSIONS.RECEIVE_SMS,
+				]);
 
-    requestSMSPermission();
-  }, []);
+				if (
+					granted["android.permission.READ_SMS"] ===
+						PermissionsAndroid.RESULTS.GRANTED &&
+					granted["android.permission.RECEIVE_SMS"] ===
+						PermissionsAndroid.RESULTS.GRANTED
+				) {
+					console.log("SMS permission granted");
+					checkPermissions();
+				} else {
+					console.log("SMS permission denied");
+				}
+			} catch (error) {
+				console.log("Error requesting SMS permission:", error);
+			}
+		};
 
-  useEffect(() => {
-    const handleSmsReceived = (status, sms, error) => {
-      if (status === 'Start Read SMS successfully' || status === 'success') {
-        console.log('SMS received');
-        console.log('SMS:', sms);
-        // Add your SMS handling logic here
-      } else {
-        console.log('Error in success callback');
-        console.log('Error:', error);
-      }
-    };
+		checkPermissions();
+		requestSMSPermission();
+	}, []);
 
-    if (hasReceiveSMSPermission && hasReadSMSPermission) {
-      const subscription = startReadSMS(handleSmsReceived);
-      return () => {
-        stopReadSMS();
-        subscription.remove();
-      };
-    }
-  }, [hasReceiveSMSPermission, hasReadSMSPermission]);
+	useEffect(
+		() => {
+			// const handleSmsReceived = (status, sms, error) => {
+			//   if (status === 'Start Read SMS successfully' || status === 'success') {
+			//     console.log('SMS received');
+			//     console.log('SMS:', sms);
+			//     // Add your SMS handling logic here
+			//   } else {
+			//     console.log('Error in success callback');
+			//     console.log('Error:', error);
+			//   }
+			// };
+			// if (hasReceiveSMSPermission && hasReadSMSPermission) {
+			//   const subscription = startReadSMS(handleSmsReceived);
+			//   return () => {
+			//     stopReadSMS();
+			//     subscription.remove();
+			//   };
+			// }
+		},
+		[
+			// hasReceiveSMSPermission,
+			// hasReadSMSPermission
+		]
+	);
 
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Code Verification Screen</Text>
-      <View style={styles.otpContainer}>
-        {[0, 1, 2, 3].map((index) => (
-          <TextInput
-            key={index}
-            style={styles.otpInput}
-            onChangeText={(text) => handleVerificationCodeChange(index, text)}
-            value={verificationCode[index]}
-            maxLength={1}
-            keyboardType="numeric"
-          />
-        ))}
-      </View>
-      <Button title="Verify" onPress={handleVerifyButton} />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Code Verification Screen</Text>
+			<View style={styles.otpContainer}>
+				{[0, 1, 2, 3].map((index) => (
+					<TextInput
+						key={index}
+						style={styles.otpInput}
+						onChangeText={(text) => handleVerificationCodeChange(index, text)}
+						value={verificationCode[index]}
+						maxLength={1}
+						keyboardType="numeric"
+					/>
+				))}
+			</View>
+			<Button title="Verify" onPress={handleVerifyButton} />
+		</View>
+	);
 };
 
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#E0E0E0',
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  otpContainer: {
-    flexDirection: 'row',
-    marginBottom: 20,
-  },
-  otpInput: {
-    flex: 1,
-    height: 50,
-    borderWidth: 1,
-    borderColor: 'gray',
-    marginHorizontal: 5,
-    textAlign: 'center',
-    fontSize: 24,
-    borderRadius: 8,
-  },
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "#E0E0E0",
+	},
+	title: {
+		fontSize: 18,
+		fontWeight: "bold",
+		marginBottom: 20,
+	},
+	otpContainer: {
+		flexDirection: "row",
+		marginBottom: 20,
+	},
+	otpInput: {
+		flex: 1,
+		height: 50,
+		borderWidth: 1,
+		borderColor: "gray",
+		marginHorizontal: 5,
+		textAlign: "center",
+		fontSize: 24,
+		borderRadius: 8,
+	},
 });
 
 export default OTPScreen;
