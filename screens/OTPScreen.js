8f0d9820cef58b2f1425113b2a91512984523b36
Abubakar Almuhandis <89389485+IAmalmuhandis@users.noreply.github.com>
2023-05-16 07:34:36 +0100

    made some changes

--- screens/OTPScreen.js
+++ screens/OTPScreen.js
@@ -1,15 +1,37 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, TextInput, Button, StyleSheet } from "react-native";
-import { PermissionsAndroid } from "react-native";
-import {
-	startReadSMS,
-	stopReadSMS,
-} from "@maniac-tech/react-native-expo-read-sms";
+import useApp from "../useApp";
+
+
+const PermissionStatus = ({
+    READ_SMS_PERMISSION_STATUS,
+    RECEIVE_SMS_PERMISSION_STATUS,
+    requestReadSMSPermission,
+  }) => {
+    console.log(
+      "READ_SMS_PERMISSION_STATUS, RECEIVE_SMS_PERMISSION_STATUS:",
+      READ_SMS_PERMISSION_STATUS,
+      RECEIVE_SMS_PERMISSION_STATUS
+    );
+  };
+
 
 const OTPScreen = () => {
 	const [verificationCode, setVerificationCode] = useState(["", "", "", ""]);
-	const [hasReceiveSMSPermission, setHasReceiveSMSPermission] = useState(false);
-	const [hasReadSMSPermission, setHasReadSMSPermission] = useState(false);
+    const {
+        appState,
+        buttonClickHandler,
+        checkPermissions,
+        errorCallbackStatus,
+        hasReceiveSMSPermission,
+        hasReadSMSPermission,
+        requestReadSMSPermission,
+        smsPermissionState,
+        successCallbackStatus,
+        smsValue,
+        smsError,
+      } = useApp();
+      console.log(smsValue);
 
 	const handleVerificationCodeChange = (index, value) => {
 		const updatedVerificationCode = [...verificationCode];
@@ -17,7 +39,6 @@ const OTPScreen = () => {
 		setVerificationCode(updatedVerificationCode);
 	};
 
-<<<<<<< HEAD
   const handleVerifyButton = () => {
     const code = verificationCode.join('');
     console.log('Entered code:', code);
@@ -26,73 +47,7 @@ const OTPScreen = () => {
     // Verify the entered code
     // You can implement your code verification logic here
   };
-
-  useEffect(() => {
-    const checkPermissions = async () => {
-      const customHasReceiveSMSPermission = await PermissionsAndroid.check(
-        PermissionsAndroid.PERMISSIONS.RECEIVE_SMS
-      );
-      const customHasReadSMSPermission = await PermissionsAndroid.check(
-        PermissionsAndroid.PERMISSIONS.READ_SMS
-      );
-
-      setHasReceiveSMSPermission(customHasReceiveSMSPermission);
-      setHasReadSMSPermission(customHasReadSMSPermission);
-    };
-
-    const requestSMSPermission = async () => {
-      try {
-        const granted = await PermissionsAndroid.requestMultiple([
-          PermissionsAndroid.PERMISSIONS.READ_SMS,
-          PermissionsAndroid.PERMISSIONS.RECEIVE_SMS,
-        ]);
-
-        if (
-          granted['android.permission.READ_SMS'] === PermissionsAndroid.RESULTS.GRANTED &&
-          granted['android.permission.RECEIVE_SMS'] === PermissionsAndroid.RESULTS.GRANTED
-        ) {
-          console.log('SMS permission granted');
-          checkPermissions();
-        } else {
-          console.log('SMS permission denied');
-        }
-      } catch (error) {
-        console.log('Error requesting SMS permission:', error);
-      }
-    };
-
-    requestSMSPermission();
-  }, []);
-
-  useEffect(() => {
-    const handleSmsReceived = (sms) => {
-      console.log('SMS received');
-      console.log('SMS:', sms);
-
-      // Extract the verification code from the SMS
-      const regex = /(\d{4})/; // assuming the verification code is a 4-digit number
-      const match = sms.match(regex);
-      if (match) {
-        const code = match[1];
-        console.log('Autofilled code:', code);
-        setVerificationCode(code.split('')); // Update the state with the received code
-      }
-    };
-
-    const handleError = (error) => {
-      console.log('Error in SMS callback');
-      console.log('Error:', error);
-    };
-
-    if (hasReceiveSMSPermission && hasReadSMSPermission) {
-      const subscription = startReadSMS(handleSmsReceived, handleError);
-      return () => {
-        stopReadSMS();
-        subscription.remove();
-      };
-    }
-  }, [hasReceiveSMSPermission, hasReadSMSPermission]);
-
+  
 
   return (
     <View style={styles.container}>
@@ -112,33 +67,8 @@ const OTPScreen = () => {
       <Button title="Verify" onPress={handleVerifyButton} />
     </View>
   );
-=======
-	const handleVerifyButton = () => {
-		const code = verificationCode.join("");
-		// Verify the entered code
-		// You can implement your code verification logic here
-	};
 
-	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Code Verification Screen</Text>
-			<View style={styles.otpContainer}>
-				{[0, 1, 2, 3].map((index) => (
-					<TextInput
-						key={index}
-						style={styles.otpInput}
-						onChangeText={(text) => handleVerificationCodeChange(index, text)}
-						value={verificationCode[index]}
-						maxLength={1}
-						keyboardType="numeric"
-					/>
-				))}
-			</View>
-			<Button title="Verify" onPress={handleVerifyButton} />
-		</View>
-	);
->>>>>>> c4ada8b875922f940bfc16a18a79fd0826ed7bc1
-};
+        }
 
 const styles = StyleSheet.create({
 	container: {
